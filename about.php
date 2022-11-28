6f74c90e7ee36e9e679011a3d5e3799565488c6f
Arron Mixell <arronmixell@yahoo.com>
2022-11-28 12:04:37 -0500

    it is done

---
+++ about.php
@@ -0,0 +1,8 @@
+
+    <?php include 'inc/header.php'; ?>
+    
+    <h2>About</h2>
+    <img src="/bigcoffeefeedback/img/big-coffee.png" class="w-25 mb-3" alt="img-of-big-coffee">
+    <p class="text-center">Here at Big Coffee, we strive to go above and beyond expectations and deliver a great coffee experience. Please tell us how we are doing through this feedback portal. Thank you.</p>
+
+    <?php include 'inc/footer.php'; ?>
